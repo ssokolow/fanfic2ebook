3c4caaa99e91f90c5217156149176f2a8be879dd
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2011-03-23 00:16:02 -0400

    Bring setup.py up to spec for non-py2exe use.

--- setup.py
+++ setup.py
@@ -1,24 +1,34 @@
-import os
+#!/usr/bin/env python
+import sys
 from distutils.core import setup
 
 try:
-	import py2exe
+   import py2exe
 except ImportError:
-	pass
+   pass
 
-setup(
-	scripts=['src/fanfic2html', 'src/fanfic2lrf'],
-	console=['src/fanfic2html'],
-	options={
-		'py2exe': 
-		{
-			'bundle_files': 1,
-			'compressed': True,
-			'includes': ['lxml.etree', 'lxml._elementpath', 'gzip'],
-			'optimize': 2,
-		}
-	},
-	packages = ['fanfic2ebook'],
-	package_dir = {'': 'src'},
-	zipfile = None
+sys.path.insert(0, 'src')
+from fanfic2ebook import __version__
+
+setup(name='fanfic2ebook',
+    version=__version__,
+    description="Fanfiction downloader/cleaner for offline reading",
+    author='Stephan Sokolow',
+    url='https://github.com/ssokolow/fanfic2ebook',
+
+   scripts=['src/fanfic2html', 'src/fanfic2lrf'],
+   packages = ['fanfic2ebook'],
+   package_dir = {'': 'src'},
+
+   console=['src/fanfic2html'],
+   options={
+      'py2exe':
+      {
+         'bundle_files': 1,
+         'compressed': True,
+         'includes': ['lxml.etree', 'lxml._elementpath', 'gzip'],
+         'optimize': 2,
+      }
+   },
+   zipfile = None
 )
