7d986d8ff888dc6a03423591d6acd75ed0eaeae4
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2011-10-30 10:00:53 -0500

    Move all possible py2exe bits out of batch files and into setup.py

--- make_exe.bat
+++ make_exe.bat
@@ -1,15 +1,7 @@
 @echo off
-:: Simple script for efficient EXE generation.
-:: Adapted from http://www.py2exe.org/index.cgi/WinBatch
-:: (And then pruned down when I got fed up debugging "The syntax of the command is incorrect")
 
 :: Limit environment changes to this file
 setlocal
-
-:: Set paths to required commands
-::TODO: Figure out how to look these up at runtime.
-set SevenZipEXE="C:\Program Files\7-Zip\7z.exe"
-set UpxEXE=C:\Tools\upx\upx.exe
 
 :: Optimize ALL included code without hard-coding a path to Python.
 :: http://www.py2exe.org/index.cgi/OptimizedBytecode
@@ -17,37 +9,3 @@ set PYTHONOPTIMIZE=2
 
 ::Compile the Python-Script
 setup.py py2exe
-if not "%errorlevel%"=="0" (
-        echo Py2EXE Error!
-        pause
-        goto:eof
-)
-
-:: Clean out Py2EXE intermediate products
-rd build /s /q
-
-:: Recompress content with 7-Zip's Deflate engine
-echo.
-echo "TODO: Is it possible to UPX-pack DLL/PYDs in the bundle before recompressing with 7-zip?"
-echo "TODO: Recompress content with 7-zip."
-echo.
-::%SevenZipEXE% -aoa x "%~dpn0_EXE\library.zip" -o"%~dpn0_EXE\library\"
-::del "%~dpn0_EXE\library.zip"
-::
-::cd %~dpn0_EXE\library\
-::%SevenZipEXE% a -tzip -mx9 "..\library.zip" -r
-::cd..
-::rd "%~dpn0_EXE\library" /s /q
-
-:: Compress runtime with UPX
-echo.
-echo "Compressing runtime with UPX..."
-echo.
-cd dist\
-%UpxEXE% --best *.*
-
-:: Done
-echo.
-echo.
-echo "Done."
-echo.
--- setup.py
+++ setup.py
@@ -7,13 +7,128 @@
 @todo: Test against Python 3 and add "Programming Language :: Python :: 3" to the classifiers
 @todo: Decide on a version scheme py2exe is OK with (http://docs.python.org/distutils/setupscript.html#additional-meta-data)
 """
-import sys
+import os, subprocess, sys
 from distutils.core import setup
+from distutils import log
 
+def advzip(paths, description=None):
+    """Wrapper for AdvanceCOMP's advzip.exe.    
+    
+    Assumes it will either be in the PATH or the current directory.
+    """
+    cmd = ['advzip', '-z4']
+    desc = description or os.path.basename(paths)
+    
+    if isinstance(paths, basestring):
+        paths = [paths]
+
+    log.info("*** recompressing %s ***", desc)
+    try:
+        cmd.extend(paths)
+        subprocess.call(cmd)
+    except Exception, err:
+        log.warn("WARNING: Failed to recompress %s:\n\t%s\nDid you put copies "
+            "of advzip.exe and zlib.dll next to your setup.py?\n",
+            desc, err)
+
+def upx(paths, description=None):
+    """Wrapper for upx.exe.
+    
+    Assumes it will either be in the PATH or the current directory.
+    """
+    cmd = ['upx', '--best']
+    desc = description or os.path.basename(paths)
+    
+    if isinstance(paths, basestring):
+        paths = [paths]
+
+    log.info("*** upx-compress %s ***", desc)
+    try:
+        cmd.extend(paths)
+        subprocess.call(cmd)
+    except Exception, err:
+        log.warn("WARNING: Failed to UPX-pack %s\n\t%s\n"
+            "Did you put a copy of upx.exe next to your setup.py?\n", 
+            desc, err)
+
+commands = {}
 try:
-   import py2exe
+    import py2exe
+    from py2exe.build_exe import py2exe
+    
+    class CompactingPy2exe(py2exe):
+        """Extend py2exe to support various additional
+        ways for compressing the output of the process.
+        
+        New py2exe options:
+        - upx_internals:
+          - If set to 1, UPX-pack python??.dll.
+          - If set to 2, also UPX all compiled modules. 
+            (Currently broken on Py27)
+        - upx_results: If True, UPX resulting EXEs.
+        - recompress_zip: If True, pass library.zip through AdvanceCOMP
+        
+        upx_internals parts borrowed from
+        http://www.py2exe.org/index.cgi/BetterCompression
+        
+        @todo: Figure out why UPX compression of .pyd files causes crashes.
+        @todo: Figure out why this suddenly is only getting called for DLLS
+               and PYDs with a result of copied=0.
+        """
+        def initialize_options(self):
+            # Add a new "upx" option for compression with upx
+            py2exe.initialize_options(self)
+            self.upx_internals = 0
+            self.upx_exclude = []
+            self.upx_results = False
+            self.recompress_zip = False
+
+        def copy_file(self, *args, **kwargs):
+            # Override to UPX copied binaries.
+            (fname, copied) = result = py2exe.copy_file(self, *args, **kwargs)
+
+            basename = os.path.basename(fname)
+            if (copied and self.upx_internals >= 2 and
+                (basename[:6]+basename[-4:]).lower() != 'python.dll' and
+                fname[-4:].lower() in ('.dll', '.pyd') and
+                basename.lower() not in self.upx_exclude):
+                    upx(os.path.normpath(fname))
+            return result
+
+        def create_binaries(self, *args, **kwargs):
+            py2exe.create_binaries(self, *args, **kwargs)
+            if self.upx_results:
+                upx(self.console_exe_files +
+                    self.windows_exe_files +
+                    self.service_exe_files,
+                    'output files')
+    
+        def make_lib_archive(self, *args, **kwargs):
+            zip_filename = py2exe.make_lib_archive(self, *args, **kwargs)
+            try:
+                if self.recompress_zip:
+                    advzip(os.path.normpath(zip_filename), 'library.zip')
+            finally:
+                return zip_filename
+
+        def patch_python_dll_winver(self, dll_name, new_winver=None):
+            # Override this to first check if the file is upx'd and skip if so
+            if not self.dry_run:
+                if not subprocess.call(['upx', '-qt', dll_name], 
+                                       stdout=open(os.devnull, 'w')):
+                    if self.verbose:
+                        log.info("Skipping setting sys.winver for '%s' (UPX'd)",
+                            dll_name)
+                else:
+                    py2exe.patch_python_dll_winver(self, dll_name, new_winver)
+                    # We UPX this one file here rather than in copy_file so
+                    # the version adjustment can be successful
+                    if self.upx_internals >= 1:
+                        upx(dll_name)
+                        
+    commands['py2exe'] = CompactingPy2exe
 except ImportError:
-   pass
+    pass
 
 sys.path.insert(0, 'src')
 from fanfic2ebook import __version__
@@ -40,8 +155,9 @@ setup(name='fanfic2ebook',
     scripts=['src/fanfic2html', 'src/fanfic2lrf'],
     packages = ['fanfic2ebook'],
     package_dir = {'': 'src'},
-
-    console=['src/fanfic2html'],
+    cmdclass = commands,
+    
+    windows=['src/fanfic2html'],
     options={
         'py2exe':
         {
@@ -54,6 +170,9 @@ setup(name='fanfic2ebook',
             ],
             'includes': ['lxml.etree', 'lxml._elementpath', 'gzip'],
             'optimize': 2,
+            'recompress_zip': True,
+            'upx_results': True,
+            'upx_internals': 1,
         }
     },
     zipfile = None
